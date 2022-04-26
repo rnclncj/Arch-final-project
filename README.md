b5f020d4a8acd3a0352b3640a32aaeb0d8b6de4b
Jasper Lee <jasperlee@wireless-10-148-121-91.public.utexas.edu>
2022-04-26 15:17:27 -0500

    got a few more things started

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
+
 # Arch-final-project
-!!!Visualizing Verilog!!!
+!!!!!Visualizing Verilog!!!!!
+
+"wire/reg" name ADD arg1 arg2...
+
+wire b = 1;
+wire a = b + c + d;
+
+wire b = 1
+wire (b + c) + b c
+wire a ADD (b + c) d
+
+wire .temp0 + b c
+wire a + .temp0 d
+
---
+++ Visualizer.java
@@ -0,0 +1,49 @@
+// https://zetcode.com/gfx/java2d/
+
+import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+// import javax.swing.JFrame;
+// import javax.swing.JPanel;
+import javax.swing.*;
+
+class Surface extends JPanel {
+
+    private void doDrawing(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.drawString("Java 2D Java 2D Java 2D Java 2D Java 2D Java 2D Java 2D Java 2D Java 2D Java 2D Java 2D Java 2D Java 2D Java 2D Java 2D Java 2D Java 2D Java 2D Java 2D Java 2D Java 2D Java 2D Java 2D Java 2D Java 2D Java 2D Java 2D Java 2D Java 2D Java 2D Java 2D Java 2D Java 2D Java 2D Java 2D Java 2D Java 2D Java 2D Java 2D Java 2D Java 2D Java 2D Java 2D Java 2D Java 2D Java 2D Java 2D", 50, 50);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        doDrawing(g);
+    }
+}
+
+public class Visualizer extends JFrame {
+
+    public Visualizer() {
+        initUI();
+    }
+
+    private void initUI() {
+        add(new Surface());
+
+        setTitle("Verilog Visualizer");
+        setSize(3300, 3200);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // add(new JScrollPane());
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                Visualizer vis = new Visualizer();
+                vis.setVisible(true);
+            }
+        });
+    }
+}
