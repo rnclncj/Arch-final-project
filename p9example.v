8d01e6e495df7965bfcbe82d5d5f364088b3c3ac
Lukeluds <luyuan925@163.com>
2022-04-29 23:32:10 -0500

    michelle changed p9example

--- p9example.v
+++ p9example.v
@@ -1,9 +1,12 @@
-// ji
-wire a = 1; 
-wire b = a;
-wire c = a;
-wire d = ~a;
-wire e = a+b+c+d;
-wire f = {4,a};
-wire g = {4{f,a,b},c,d,e};
-assign b = (a == 4) ? c+a : d*~a 
+// setup
+wire a = 2'b10; 
+wire b;
+wire c;
+// testing operators
+wire d = a[1:0];
+wire e = (a+b)/(c/d);
+wire f = !!a;
+wire g = ~c;
+
+// wire g = {4{f,a,b},c,d,e};
+// assign b = (a == 4) ? c+a : d*~a 
