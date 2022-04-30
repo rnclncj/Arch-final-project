7a2a5dbdeea6ec5e8189486ef995e7fa0cee65ae
Lukeluds <luyuan925@163.com>
2022-04-29 22:58:15 -0500

    jasper commented out a line!

--- Visualizer.java
+++ Visualizer.java
@@ -154,7 +154,7 @@ class Panel extends JPanel {
     private HashMap<String, Element> elementMap;
 
     public Panel(HashMap<String, Element> em) {
-        setBackground(new Color(238, 238, 238));
+        // setBackground(new Color(238, 238, 238));
         elementMap = em;
     }
 
