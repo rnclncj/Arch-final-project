724551636e36ccf6b0e16e8b25dbe71c5f76274f
Cooper Wilk <cooper.wilk@utexas.edu>
2022-05-01 16:09:48 -0500

    reorder correctly and split into two files

---
+++ Element.java
@@ -0,0 +1,213 @@
+import java.awt.*;
+import javax.swing.*;
+import java.io.*;
+import java.util.*;
+
+public class Element {
+    private String name;
+    private String type;
+    private int width;
+    private String operation;
+    private ArrayList<String> operands;
+    private int colNum;
+    private int yCoord;
+    private int lastCol;
+
+    public Element(String line) {
+        StringTokenizer tokenizer = new StringTokenizer(line);
+        type = tokenizer.nextToken();
+        name = tokenizer.nextToken();
+        width = 1;
+        if(name.contains("]")){
+            width = Integer.parseInt(name.substring(name.indexOf("[")+1, name.indexOf("]")));
+            name = name.substring(name.indexOf("]")+1);
+        }
+        operation = tokenizer.nextToken();
+        operands = new ArrayList<>();
+        colNum = -1;
+        yCoord = 0;
+        while (tokenizer.hasMoreTokens()) {
+            operands.add(tokenizer.nextToken());
+        }
+        lastCol = 0;
+    }
+
+    public Element(Element elem, int cn){
+        this.name = elem.name;
+        this.type = elem.type;
+        operation = "->";
+        operands = new ArrayList<>();
+        operands.add(name);
+        this.colNum = cn;
+        lastCol = 0;
+    }
+
+    public String toString() {
+        String str = name + " " + type + " " + operation + " " + operands;
+        return str;
+    }
+
+    public int getMaxColOfInputs(HashMap<String, Element> elementMap) {
+        int max = -1;
+        for (String input : operands) {
+            if (!elementMap.containsKey(input))
+                continue;
+            int currCol = elementMap.get(input).getColNum();
+            max = Math.max(currCol, max);
+        }
+        return max;
+    }
+
+    public int getXCoord() {
+        return Visualizer.HORIZ_DIST + getColNum() * (Visualizer.FULL_WIDTH + Visualizer.HORIZ_DIST);
+    }
+
+    // returns the height of the element
+    public int getHeight() {
+        if(operation.equals("->"))
+            return Visualizer.BASE_HEIGHT / 4;
+        return Visualizer.BASE_HEIGHT * getOperands().size();
+    }
+
+    // returns the height of the element
+    public int getRealBaseHeight() {
+        return getHeight() / operands.size();
+    }
+
+    public int getOutX() {
+        return getXCoord() + Visualizer.FULL_WIDTH;
+    }
+
+    public int getOutY() {
+        return yCoord + getHeight() / 2;
+    }
+
+    public void setYCoord(int yCoord) {
+        this.yCoord = yCoord;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getOperation() {
+        return operation;
+    }
+
+    public void setOperation(String operation) {
+        this.operation = operation;
+    }
+
+    public ArrayList<String> getOperands() {
+        return operands;
+    }
+
+    public void setOutputPoint(int colNum, int yCoord) {
+        this.colNum = colNum;
+        this.yCoord = yCoord;
+    }
+
+    public int getColNum() {
+        return colNum;
+    }
+
+    public void setColNum(int colNum) {
+        this.colNum = colNum;
+    }
+
+    public int getYCoord() {
+        return yCoord;
+    }
+
+    public int getYBase() {
+        return yCoord + getHeight();
+    }
+
+    public int getLastCol() {
+        return lastCol;
+    }
+
+    public void setLastCol(int lc) {
+        lastCol = lc;
+    }
+
+    public int getWidth(){
+        return width;
+    }
+
+    public void setWidth(int width){
+        this.width = width;
+    }
+
+    public void draw(Graphics g, HashMap<String, Element> elementMap, ArrayList<HashMap<String, Element>> columnMaps) {
+        Graphics2D g2d = (Graphics2D) g;
+
+        if ((getOperation().equals("=") && !getType().equals("reg")) || getOperation().equals("->")) {
+            g2d.drawLine(getXCoord(), getYCoord() + getHeight() / 2, getXCoord() + Visualizer.BOX_WIDTH,
+                    getYCoord() + getHeight() / 2);
+        } else if (!getOperation().equals("<-")) {
+            g2d.drawRect(getXCoord(), getYCoord(), Visualizer.BOX_WIDTH, getHeight()); //draws box
+            if (type.equals("reg")) {
+                int base = getYBase();
+                int peakY = base - Visualizer.BOX_WIDTH / 6;
+                int peakX = getXCoord() + Visualizer.BOX_WIDTH / 2;
+                g2d.drawLine(peakX, peakY, getXCoord() + Visualizer.BOX_WIDTH / 3, base);
+                g2d.drawLine(peakX, peakY, getXCoord() + 2 * Visualizer.BOX_WIDTH / 3, base);
+            }
+        }
+
+        if (getOperation().equals("<-")) {
+            g2d.drawString(getOperands().get(0), getXCoord() + Visualizer.FULL_WIDTH / 2 - 5,
+                    getYCoord() + getHeight() / 2 - 5);
+        } else if (!getOperation().equals("->")) {
+            g2d.setFont(new Font(g2d.getFont().getName(), Font.PLAIN, 12)); 
+            g2d.drawString(getOperation(), getXCoord() + Visualizer.BOX_WIDTH / 2 - 3*getOperation().length(), getYCoord() + getHeight() / 2 + 4);
+        }
+        //draw stud line
+        g2d.drawLine(getXCoord()+Visualizer.BOX_WIDTH, getYCoord() + getHeight() / 2, getXCoord() + Visualizer.FULL_WIDTH,
+                    getYCoord() + getHeight() / 2);
+        //draws name
+        if (!(getName().charAt(0) == '.' || getOperation().equals("->"))) {
+            g2d.setFont(new Font(g2d.getFont().getName(), Font.PLAIN, 9)); 
+            String name = Visualizer.condenseName(getName(), 10);
+            g2d.drawString(name, getXCoord() + Visualizer.BOX_WIDTH + Visualizer.STUD_WIDTH / 2 - 2*name.length(), getYCoord() + getHeight() / 2 + 10); //draw name
+            g2d.drawLine(getXCoord()+Visualizer.BOX_WIDTH + Visualizer.STUD_WIDTH / 2 + 1, getYCoord() + getHeight() / 2 - 3, 
+                    getXCoord()+Visualizer.BOX_WIDTH + Visualizer.STUD_WIDTH / 2 - 1, getYCoord() + getHeight() / 2 + 2);
+            g2d.drawString(""+getWidth(), getXCoord() + Visualizer.BOX_WIDTH + Visualizer.STUD_WIDTH / 2 - 3*(int)(Math.log10(getWidth())), getYCoord() + getHeight() / 2 - 5); //draw name
+
+        }
+
+        // input wires
+        if (!getOperation().equals("<-")) {
+            ArrayList<String> operands = getOperands();
+            for (int i = 0; i < operands.size(); i++) {
+                int operandX = getXCoord();
+                int operandY = getYCoord() + (int) (getRealBaseHeight() * (i + 0.5));
+                
+                if(elementMap.get(operands.get(i)) == null)
+                    System.out.println(operands.get(i));
+                Element fromElem;
+                if (elementMap.get(operands.get(i)).getColNum() >= getColNum()) {
+                    fromElem = elementMap.get(operands.get(i));
+                } else {
+                    fromElem = columnMaps.get(getColNum() - 1).get(operands.get(i));
+                }
+                int inputX = fromElem.getOutX();
+                int inputY = fromElem.getOutY();
+                g2d.drawLine(inputX, inputY, operandX, operandY);
+            }
+        }
+    }
+}
