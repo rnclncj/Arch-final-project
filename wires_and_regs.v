6774340d78997cdc473fbcebde7e96cf784b304b
Michelle Ding <mding01@cs.utexas.edu>
2022-05-05 14:51:59 -0500

    changed test

--- wires_and_regs.v
+++ wires_and_regs.v
@@ -1,11 +1,10 @@
-wire halt;
+wire halt = 1;
 reg WB_valid;
-reg WB_flush;
-reg M2_valid;
-reg WB_stall;
-reg WB_stall;
+reg WB_flush = 1;
+reg M2_valid = 1;
+reg WB_stall = 0;
 reg WB_isAdrp;
-reg M2_isAdrp;
+reg M2_isAdrp = 1;
 
 always @(posedge clk) begin
     if (~halt) begin
@@ -14,6 +13,5 @@ always @(posedge clk) begin
         if (~WB_stall) begin
             WB_isAdrp <= M2_isAdrp;
         end
-        else if ()
     end
 end
